8a18978a5bfe8ca1dc9bb2c682ada98b405e4fb1
Stian Lystad <Stianlystad_1@hotmail.com>
2022-04-02 11:42:35 +0000

    Starting out with suggestions from CS

--- run.py
+++ run.py
@@ -1,3 +1,39 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+scores = {"computer": 0, "player" : 0}
+
+
+class Board:
+    """
+    Main board class. Sets board size , the number of ships, the player's name and board type (player board or computer)
+    has methods for adding ships and guesses and printing the board
+    """
+
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+
+    def print(self):
+        for row in self.board:
+            print(" ".join(row))
+
+        if (x, y) in self.ships:
+            self.board[x] [y] = "*"
+            return "Hit"
+        else:
+            return "Miss"
+
+    
+    def add_ship(self, x, y, type="computer"):
+        if len(self.ships) >= self.num_ships:
+            print("Error: you cannot add more ships!")
+        else:
+            self.ships.append((x, y))
+            if self.type == "player":
+                self.board[x] [y] = "@"
+
